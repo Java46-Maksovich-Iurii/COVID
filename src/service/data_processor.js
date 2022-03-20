082bba3a1ac5fda9b69cb8dbdb575888c6e5f5d2
Iurii_Maksovich <gizmaksovitch@gmail.com>
2022-03-20 22:25:25 +0200

    Added correct format table numbers

--- src/service/data_processor.js
+++ src/service/data_processor.js
@@ -5,8 +5,8 @@ export default class DataProcessor {
     }
 
     async getAllFormatData() {
-        const c = await this.#serverData
-        const data = Object.entries(c).map(obj => {
+        const promiseData = await this.#serverData;
+        const data = Object.entries(promiseData).map(obj => {
             return {
                 "continent": obj[0],
                 "confirmedCases": this.#getConfirmedCases(obj[1]),
@@ -19,12 +19,12 @@ export default class DataProcessor {
     #getConfirmedCases(continentData) {
         const confirmedCases = continentData.reduce((casesAcc, currV) => {casesAcc+=currV.confirmed; return casesAcc}, 0);
         const population = continentData.reduce((populationAcc, currV) => {populationAcc+=currV.population; return populationAcc}, 0);
-        return (confirmedCases/population) * 100;
+        return ((confirmedCases/population) * 100).toFixed(2);
     }
     #getDeathCases(continentData) {
         const confirmedDeath = continentData.reduce((deathAcc, currV) => {deathAcc+=currV.deaths; return deathAcc}, 0);
         const population = continentData.reduce((populationAcc, currV) => {populationAcc+=currV.population; return populationAcc}, 0);
-        return (confirmedDeath/population) * 100;
+        return ((confirmedDeath/population) * 100).toFixed(2);
     }
 
 
